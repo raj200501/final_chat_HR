5baca80128bb3682f7c68a452263711133c68383
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-26 21:48:15 -0700

    Update langchain_model.py

--- langchain_model.py
+++ langchain_model.py
@@ -6,12 +6,14 @@ from langchain.llms import OpenAI  # Correct import for OpenAI LLM
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+
 def read_text_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
     return content
 
-# Load mental health advice from the text file
+
+
 text_file_path = 'new.txt'
 mental_health_advice = read_text_file(text_file_path)
 
@@ -20,8 +22,9 @@ max_advice_length = 1000  # Adjust this value based on your needs
 truncated_advice = mental_health_advice[:max_advice_length]
 
 # Initialize LangChain components
-llm = OpenAI(api_key='put key here')  # Replace with your OpenAI API key
+llm = OpenAI(api_key='key')  # Replace with your OpenAI API key
 conversation = ConversationChain(llm=llm)
+
 
 @app.route('/chat', methods=['POST'])
 def chat():
@@ -36,6 +39,7 @@ def chat():
     response = conversation.run(input=combined_input)
     
     return jsonify({'response': response})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
